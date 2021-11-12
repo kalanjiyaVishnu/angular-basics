7ea07a087c9f08f1747d5db282d4fbc728fa0440
r1558 <kalanjiya.vishnu01@gmail.com>
2021-11-12 18:54:39 +0530

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"home",
+  component:HomeComponent
+},{
+  path:"login",
+  component:LoginComponent
+},{
+  path:"contact",
+  component:ContactComponent
+},{
+  path:"about",
+  component:AboutComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
